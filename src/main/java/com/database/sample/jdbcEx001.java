7aba05c2b969ddbb0f16db3460c70e5fc26101f3
ehwlfk2 <ehwlfk2@gmail.com>
2021-08-27 16:02:18 +0900

    update pom.xml, database file root, index.html

---
+++ src/main/java/com/database/sample/jdbcEx001.java
@@ -0,0 +1,7 @@
+package main.java.com.database.sample;
+
+public class jdbcEx001 {
+	public static void main(String[] args) {
+		System.out.println("Hello~ This is Database File");
+	}
+}
---
+++ src/main/java/com/database/util/ServiceUtil.java
@@ -0,0 +1,25 @@
+package main.java.com.database.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ServiceUtil {
+	static Connection con;
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Success!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "C##APPLE", "1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
